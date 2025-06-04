a653096528fa94ffa6cbb6a0648721998f3a3901
AlinaGay <gayfullina.a@gmail.com>
2025-06-04 20:05:02 +0200

    Add models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,76 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+SLUG_MAX_LENGTH = 50
+NAME_MAX_LENGTH = 256
+
+
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = (
+        (ADMIN, 'Администратор'),
+        (USER, 'Пользователь')
+    )
+    first_name = models.CharField(max_length=NAME_MAX_LENGTH)
+    last_name = models.CharField(max_length=NAME_MAX_LENGTH)
+    username = models.CharField(max_length=NAME_MAX_LENGTH)
+    role = models.CharField(
+        max_length=max(len(role) for role, _ in ROLE_CHOICES),
+        choices=ROLE_CHOICES,
+        default=USER
+    )
+    email = models.EmailField(unique=True)
+    is_subscribed = models.BooleanField(default=False)
+    avatar = models.ImageField(
+        upload_to='users/avatars/',
+        null=True,
+        default=None
+    )
+
+
+class Ingredients(models.Model):
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
+    measurement_unit = models.CharField(max_length=16)
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
+    slug = models.SlugField(max_length=SLUG_MAX_LENGTH, unique=True)
+
+
+class Recipes(models.Model):
+    tags = models.ManyToManyField(Tags, through='RecipeTag',
+                                  through_fields=('recipe', 'tag'))
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(Ingredients,
+                                         through='RecipeIngredient',
+                                         through_fields=('recipe',
+                                                         'ingredient'))
+    is_favorited = models.BooleanField(default=False)
+    is_in_shopping_cart = models.BooleanField(default=False)
+    name = models.CharField(max_length=NAME_MAX_LENGTH)
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        null=True,
+        default=None
+    )
+    text = models.TextField()
+    cooking_time = models.PositiveIntegerField()
+    short_link = models.URLField(default=None)
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipes, on_delete=models.SET_NULL,
+                               blank=True, null=True)
+    ingredient = models.ForeignKey(Ingredients, on_delete=-models.SET_NULL,
+                                   blank=True, null=True)
+
+
+class RecipeTag(models.Model):
+    recipe = models.ForeignKey(Recipes, on_delete=models.SET_NULL,
+                               blank=True, null=True)
+    tag = models.ForeignKey(Tags, on_delete=models.SET_NULL,
+                            blank=True, null=True)
