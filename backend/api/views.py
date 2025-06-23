9ae398f104f0c98d7ac4e74e5ceed7eaccc4d9f7
AlinaGay <gayfullina.a@gmail.com>
2025-06-23 23:55:31 +0200

    Add changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.mixins import ListModelMixin
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
