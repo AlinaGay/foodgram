6967dd7179817993e8bad0775626212a62f4c63b
AlinaGay <gayfullina.a@gmail.com>
2025-06-27 23:44:54 +0200

    Add admin models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -3,7 +3,41 @@ from django.contrib import admin
 from .models import User, Ingredient, Tag, Recipe
 
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'last_name',
+        'first_name',
+        'role',
+        'email'
+    )
+
+    list_iditable = (
+        'last_name',
+        'first_name',
+        'email'
+    )
+
+    search_fields = ('last_name',)
+    list_filter = ('role',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug'
+    )
+
+    list_iditable = (
+        'title',
+        'slug'
+    )
+
+    search_fields = ('name',)
+    list_filter = ('name',)
+    list_display_links = ('name',)
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Ingredient)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Recipe)
