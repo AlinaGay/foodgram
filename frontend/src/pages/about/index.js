c8742096455e64fd5cb0169e709b6e9a398402ab
AlinaGay <gayfullina.a@gmail.com>
2025-08-14 22:14:26 +0200

    Add pages

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -18,7 +18,7 @@ const About = ({ updateOrders, orders }) => {
           <h2 className={styles.subtitle}>Что это за сайт?</h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Представляю вам проект, созданный во время обучения в Яндекс Практикуме. Этот проект — часть учебного курса, но он создан полностью самостоятельно.
+              Представляю вам проект, созданный во время обучения. Этот проект — часть учебного курса, но он создан полностью самостоятельно.
             </p>
             <p className={styles.textItem}>
               Цель этого сайта — дать возможность пользователям создавать и хранить рецепты на онлайн-платформе. Кроме того, можно скачать список продуктов, необходимых для
@@ -32,7 +32,7 @@ const About = ({ updateOrders, orders }) => {
             </p>
           </div>
         </div>
-        <aside>
+        {/* <aside>
           <h2 className={styles.additionalTitle}>
             Ссылки
           </h2>
@@ -41,10 +41,10 @@ const About = ({ updateOrders, orders }) => {
               Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="#" className={styles.textLink}>Алина</a>
             </p>
           </div>
-        </aside>
+        </aside> */}
       </div>
       
     </Container>
